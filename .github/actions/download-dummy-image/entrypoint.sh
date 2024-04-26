e11d39cd5f89f295291dc77dbbb8562858d44b6d
Kostyantyn Zagorodniy <kostyantyn.zagorodniy@gmail.com>
2024-04-26 23:29:13 +0200

    Update entrypoint.sh

--- .github/actions/download-dummy-image/entrypoint.sh
+++ .github/actions/download-dummy-image/entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "starting to download the dummy image: https://placehold.co/600x400/orange/olive/png"
 curl -s -S -o "file.png" https://placehold.co/600x400/orange/olive/png
 
 echo "current directory: $(pwd)"
